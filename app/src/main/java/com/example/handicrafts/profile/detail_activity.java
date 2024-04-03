0ff4f5ce800a5b43cafb35423e84c62f5e8e1c5b
Rishabh3113 <rishabhsharma2076@gmail.com>
2024-04-03 20:22:22 +0530

    rishabh commit to sanju

--- app/src/main/java/com/example/handicrafts/profile/detail_activity.java
+++ app/src/main/java/com/example/handicrafts/profile/detail_activity.java
@@ -1,6 +1,7 @@
 package com.example.handicrafts.profile;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -8,14 +9,25 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.handicrafts.R;
+import com.google.android.material.button.MaterialButton;
 
 public class detail_activity extends AppCompatActivity {
     ImageButton imageButton;
+    MaterialButton maps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        maps=findViewById(R.id.button);
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(detail_activity.this,maps_activity.class);
+                startActivity(intent);
+            }
+        });
+
 
      imageButton=findViewById(R.id.icons);
         imageButton.setOnClickListener(new View.OnClickListener() {
