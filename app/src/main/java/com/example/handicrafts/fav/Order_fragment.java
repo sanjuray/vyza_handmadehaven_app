0e843d1da4c2078365dad8d9289df23adbce0686
Rishabh3113 <rishabhsharma2076@gmail.com>
2024-04-22 12:33:19 +0530

    rishabh commit to sanju

---
+++ app/src/main/java/com/example/handicrafts/fav/Order_fragment.java
@@ -0,0 +1,22 @@
+package com.example.handicrafts.fav;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.handicrafts.R;
+
+public class Order_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
